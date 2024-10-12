f3d2b555f6a0b323c73b1f58a897d226e0fd3461
Utkarsh Raj <139266320+UTkARsh-RaJ01@users.noreply.github.com>
2024-10-12 10:55:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import requests
 
 def fetch_poster(movie_id):
-    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=7b995d3c6fd91a2284b4ad8cb390c7b8&language=en-US".format(movie_id))
+    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=YOUR_API_KEY &language=en-US".format(movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
